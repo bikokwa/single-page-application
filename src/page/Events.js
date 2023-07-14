563ede9bcbab17712618de577687143460af0d4f
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-07-14 18:19:26 +0300

    using data from a loader in route component

--- src/page/Events.js
+++ src/page/Events.js
@@ -1,29 +1,12 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
-import { useEffect, useState } from "react";
 
 const EventsPage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
+  const events = useLoaderData();
 
   return (
     <>
-      <div style={{ textAlign: "center" }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      <EventsList events={events} />
     </>
   );
 };
